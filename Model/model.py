273f0623a495dbdd7562adda387e500e577d62d8
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2022-04-14 10:44:45 +0300

    Initial commit

---
+++ Model/model.py
@@ -0,0 +1,42 @@
+import json
+
+import numpy as np
+import requests
+
+scheme = 'url,id,section,time'
+sections = ['football', 'basketball', 'soccer', 'tenis']
+
+data_accumulator_url = 'http://localhost:1234/informationrequest'
+
+
+def make_random_transactions():
+    return [('url', np.random.randint(0, 10), np.random.choice(sections), t) for t in range(100)]
+
+
+def trans_to_ids(trans):
+    trans_per_id = {}
+    for t in trans:
+        id = t['id']
+        if id not in trans_per_id.keys():
+            trans_per_id[id] = []
+        trans_per_id[id].append((t['url'], t['section'], t['clickTime']))
+    return trans_per_id
+
+
+def update_user(user_record, id, new_records):
+    new_record = new_records[id]
+    for _, theme, _ in new_record:
+        user_record[theme] = user_record[theme] + 1 if theme in user_record.keys() else 1
+    return user_record
+
+
+def get_transactions_from_db(k=10, **kwargs):
+    request = {'k':k}
+    for key,value in kwargs.items():
+        if value is not None:
+            request[key] = value
+
+    return requests.post(url=data_accumulator_url,data=request)
+
+# res = get_transactions_from_db(10,start_time=None, end_time=None, url=None, domain=None, section=None, id=None)
+# print(res.content.decode('utf-8'))
---
+++ Model/server.py
@@ -0,0 +1,60 @@
+import ast
+import json
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
+# import requests
+import requests
+
+import model
+
+preference_db_url = 'http://localhost:3000/get_user'
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length).decode('utf-8')  # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data)
+        json_data = ast.literal_eval(post_data)
+        new_records = model.trans_to_ids(json_data)
+        res = requests.get(preference_db_url,params={'user_id':12345})
+        content = res.content.decode('utf-8')
+        content = json.loads(content)
+        print(f"from Maiky:{content}")
+        print(f"from Tom: {new_records[12345]}")
+        # x = {'football': 4, 'tenis': 2, 'basketball': 1}
+        t = model.update_user(content, 12345, new_records)
+        print(t)
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+
+def run(server_class=HTTPServer, handler_class=S, port=9000):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
